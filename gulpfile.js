a77b38e957a6aaca80120c27805785709450133a
James Crosby <james@coggle.it>
2017-01-28 13:14:55 +0000

    to reduce file size omit field names in encoded file

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,10 @@ var _       = require('underscore');
 var emojiData = function(){
     var data = JSON.parse(fs.readFileSync('./bower_components/emoji-data-minimal/emoji.json'));
     // strip out the data which isn't needed:
-
+    var fields = ['name', 'unified', 'short_name', 'short_names', 'category',
+                  'sort_order', 'skin_variations', 'text', 'texts'];
     data = _.map(data, function(d){
-        d =  _.pick(
-            d,
-            'name', 'unified', 'short_name', 'short_names', 'category',
-            'sort_order', 'skin_variations', 'text', 'texts'
-        );
+        d =  _.pick(d, fields);
         // skin variations work in a standardised way, we don't need the
         // detailed data from upstream on which variations are included in
         // which emoji sets:
@@ -24,6 +21,15 @@ var emojiData = function(){
         }
         return d;
     });
+    // encode the data as an array of fields, without names:
+    data = _.map(data, function(d){
+        var r = [];
+        for(var i = 0; i < fields.length; i++){
+            r.push(d[fields[i]]);
+        }
+        return r;
+    });
+
 
     return JSON.stringify(data);
 };
